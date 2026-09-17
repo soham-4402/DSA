a7faf8b36ab0b41c99764b1bf7cb40b08be51623
soham chattar <sohamchattar4402@gmail.com>
2026-09-17 22:28:54 +0530

    reverse array

---
+++ Arrays/reverse_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void reversearray(int arr[], int size){
+    int start = 0;
+    int end = size-1;
+    while(start<end){
+            swap(arr[start],arr[end]);
+            start++;
+            end--;
+    }
+}
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout<<"The reversed array is: "<<endl;
+    reversearray(arr, size);
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
